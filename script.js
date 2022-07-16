2c03e66d5726b850e883f8107e9c7b6c0b073a2e
Derek <derekyoung_8@msn.com>
2022-07-16 00:02:07 -0400

    Create a function to draw the grid tiles

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 const mainContainer = document.getElementsByClassName('container')[0];
+let gridSize = 16;
 
-for (let i = 0; i < 256; i++) {
-    const gridTile = document.createElement('div');
-    gridTile.classList.add('grid-tile');
-    mainContainer.appendChild(gridTile);
-    gridTile.addEventListener('mouseover', () => {
-        gridTile.classList.add('grid-hover');
-    })
+createGrid(gridSize);
+
+function createGrid(gridSize)
+{
+    for (let i = 0; i < (gridSize * gridSize); i++) {
+        const gridTile = document.createElement('div');
+        gridTile.classList.add('grid-tile');
+        mainContainer.appendChild(gridTile);
+        gridTile.addEventListener('mouseover', () => {
+            gridTile.classList.add('grid-hover');
+        })
+        gridTile.style.width = `calc(100% / ${gridSize})`;
+        gridTile.style.height = `calc(80vw / ${gridSize})`;
+        gridTile.style.maxHeight = `calc(600px / ${gridSize})`;
+    }
 }
 
-// Button prompt for a new grid size
 const buttonResize = document.getElementById('button-resize');
 buttonResize.addEventListener('click', event => {
     let newGridSize = parseInt(prompt('Enter a new size for the grid (up to 100):'));
@@ -17,21 +25,10 @@ buttonResize.addEventListener('click', event => {
     if (typeof newGridSize === "number"
         && newGridSize > 0
         && newGridSize <= 100) {
-        while (mainContainer.firstChild) {
-            mainContainer.removeChild(mainContainer.lastChild);
-        }
 
-        for (let i = 0; i < (newGridSize * newGridSize); i++) {
-            const gridTile = document.createElement('div');
-            gridTile.classList.add('grid-tile');
-            mainContainer.appendChild(gridTile);
-            gridTile.addEventListener('mouseover', () => {
-                gridTile.classList.add('grid-hover');
-            })
-            gridTile.style.width = `calc(100% / ${newGridSize})`;
-            gridTile.style.height = `calc(80vw / ${newGridSize})`;
-            gridTile.style.maxHeight = `calc(600px / ${newGridSize})`;
-        }
+        mainContainer.textContent = "";
+        
+        createGrid(newGridSize);
     }
     else {
         alert('Error - Enter a whole number between 1 and 100.');
