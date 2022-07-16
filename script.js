3cd27f853d260ddff4ab5591968a17c7bc30c0a6
Derek <derekyoung_8@msn.com>
2022-07-15 23:45:13 -0400

    Add grid resizing functionality

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
 const mainContainer = document.getElementsByClassName('container')[0];
-const gridTile = document.createElement('div');
-gridTile.classList.add('grid-tile');
-
 
 for (let i = 0; i < 256; i++) {
     const gridTile = document.createElement('div');
     gridTile.classList.add('grid-tile');
     mainContainer.appendChild(gridTile);
-
+    gridTile.addEventListener('mouseover', () => {
+        gridTile.classList.add('grid-hover');
+    })
 }
 
-const tiles = document.querySelectorAll('.grid-tile');
-tiles.forEach(tile => {
-    tile.addEventListener('mouseover', () =>
-    {
-        tile.classList.add('grid-hover');
-    })
-});
-
 // Button prompt for a new grid size
-
 const buttonResize = document.getElementById('button-resize');
 buttonResize.addEventListener('click', event => {
-    
+    let newGridSize = parseInt(prompt('Enter a new size for the grid (up to 100):'));
+
+    if (typeof newGridSize === "number"
+        && newGridSize > 0
+        && newGridSize <= 100) {
+        while (mainContainer.firstChild) {
+            mainContainer.removeChild(mainContainer.lastChild);
+        }
+
+        for (let i = 0; i < (newGridSize * newGridSize); i++) {
+            const gridTile = document.createElement('div');
+            gridTile.classList.add('grid-tile');
+            mainContainer.appendChild(gridTile);
+            gridTile.addEventListener('mouseover', () => {
+                gridTile.classList.add('grid-hover');
+            })
+            gridTile.style.width = `calc(100% / ${newGridSize})`;
+            gridTile.style.height = `calc(80vw / ${newGridSize})`;
+            gridTile.style.maxHeight = `calc(600px / ${newGridSize})`;
+        }
+    }
+    else {
+        alert('Error - Enter a whole number between 1 and 100.');
+    }
 });
