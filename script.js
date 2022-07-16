69289a2a49223f064e26c29def4113831d6b5f99
Derek <derekyoung_8@msn.com>
2022-07-15 23:03:05 -0400

    Add drawing functionality on hover

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 const mainContainer = document.getElementsByClassName('container')[0];
+const gridTile = document.createElement('div');
+gridTile.classList.add('grid-tile');
 
 
-for (let i = 0; i < 256; i++)
-{
-    console.log('lol');
-    const div = document.createElement('div');
-    div.classList.add('grid-tile');
-    mainContainer.appendChild(div);
+for (let i = 0; i < 256; i++) {
+    const gridTile = document.createElement('div');
+    gridTile.classList.add('grid-tile');
+    mainContainer.appendChild(gridTile);
+
 }
+
+const tiles = document.querySelectorAll('.grid-tile');
+tiles.forEach(tile => {
+    tile.addEventListener('mouseover', () =>
+    {
+        tile.classList.add('grid-hover');
+    })
+});
+
+// Button prompt for a new grid size
+
+const buttonResize = document.getElementById('button-resize');
+buttonResize.addEventListener('click', event => {
+    
+});
